d82d0a0907bb83242d0f0de818f4246e27bfa818
嘿嘿哈 <449266367@qq.com>
2025-10-19 15:50:32 +0800

    feat: 实现客户分页、列表导出、新增/编辑功能

--- src/main/java/com/crm/service/CustomerService.java
+++ src/main/java/com/crm/service/CustomerService.java
@@ -1,7 +1,14 @@
 package com.crm.service;
 
+import com.crm.common.result.PageResult;
 import com.crm.entity.Customer;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.crm.entity.Department;
+import com.crm.query.CustomerQuery;
+import com.crm.query.IdQuery;
+import com.crm.vo.CustomerVO;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.resource.HttpResource;
 
 /**
  * <p>
@@ -12,5 +19,25 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2025-10-12
  */
 public interface CustomerService extends IService<Customer> {
+    /**
+     * 客户列表 - 分页
+     * @param query
+     * @return
+     */
+    PageResult<CustomerVO> getPage(CustomerQuery query);
+
+    /**
+     * 导出客户信息
+     * @param query
+     * @param httpResource
+     */
+    void exportCustomer(CustomerQuery query, HttpServletResponse httpResource);
+
+
+    /**
+     * 保存或更新客户信息
+     * @param
+     */
+    void saveOrUpdate(CustomerVO customerVO);
 
 }
